387ccbae87e6a6130d2d5a7d001a0e7ffeced39e
sahibsasan <isahibsasan@gmail.com>
2026-02-18 10:10:16 +0530

    updated

--- 05_conditional.js
+++ 05_conditional.js
@@ -1,13 +1,27 @@
-let age = 20;
-let country = "India";
-let text = "You can not drive";
+// let age = 20;
+// let country = "India";
+// let text = "You can not drive";
 
-if(country === "India"){
-    if(age <= 18){
-        console.log(text);
-    }
-    else{
-        text = "You can drive";
-        console.log(text);
-    }
+// if(country === "India"){
+//     if(age <= 18){
+//         console.log(text);
+//     }
+//     else{
+//         text = "You can drive";
+//         console.log(text);
+//     }
+// }
+
+let time = 10;
+if(time < 12){
+    console.log("Good Morning");
+}
+else if(time < 18 && time >= 12){
+    console.log("Good Afternoon");
+}
+else if(time < 21 && time >= 18){
+    console.log("Good Evening");
+}
+else{
+    console.log("Good Night");
 }
