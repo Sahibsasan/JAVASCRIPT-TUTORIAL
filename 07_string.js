f0f76c27438a2c2792a2a63be84422663e795bae
sahibsasan <isahibsasan@gmail.com>
2026-02-18 11:16:09 +0530

    Add string manipulation examples to JavaScript tutorial

---
+++ 07_string.js
@@ -0,0 +1,21 @@
+let text = "Hello world";
+let text2 = "My college name is 'SVIET'";
+console.log(text2);
+
+let text3 = "This is a multi-line string\nthat spans across multiple lines.";
+console.log(text3);
+
+ let text4 = 'This is Sahib\'s laptop';
+console.log(text4);
+
+let text5 = `The sum of 2 and 3 is ${2 + 3}`;
+console.log(text5);
+
+let text6 = "Hello";
+let text7 = "World";
+let combinedText = text6 + " " + text7;
+console.log(combinedText);
+
+let text8 = `This is a multi-line string 
+that spans across multiple lines.`;
+console.log(text8);
