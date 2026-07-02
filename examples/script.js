dd5becd3028f3d990450802c64e3d3e7e381ba0e
sahibsasan <isahibsasan@gmail.com>
2026-07-02 21:12:10 +0530

    Add HTML and JavaScript files for color scheme switcher project

---
+++ examples/script.js
@@ -0,0 +1,4 @@
+let h1 = document.createElement("h1")
+let body = document.querySelector("body")
+h1.innerText = "Hello!"
+body.append(h1)
--- projects/colorChange.js
+++ projects/colorChange.js
@@ -1,27 +1,8 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
+const buttons = document.querySelectorAll(".button");
+const body = document.body;
 
-buttons.forEach(function(button){
-    console.log(button)
-    button.addEventListener('click', function(e){
-        console.log(e)
-        console.log(e.target)
-
-        if(e.target.id === 'red'){
-            body.style.backgroundColor = e.target.id; 
-        }   
-        if(e.target.id === 'blue'){
-            body.style.backgroundColor = e.target.id; 
-        }
-        if(e.target.id === 'green'){
-            body.style.backgroundColor = e.target.id; 
-        }        
-        if(e.target.id === 'pink'){
-            body.style.backgroundColor = e.target.id; 
-        }        
-        if(e.target.id === 'black'){
-            body.style.backgroundColor = e.target.id; 
-        }        
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        body.style.backgroundColor = e.target.id;
     });
 });
-
